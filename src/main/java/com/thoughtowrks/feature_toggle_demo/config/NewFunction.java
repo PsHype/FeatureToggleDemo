f8ae615bd15498ed0fdf07b498acbf27848036ff
shaoshuai <shshao@thoughtworks.com>
2017-12-04 10:45:27 +0800

    add Feature Group annotation

---
+++ src/main/java/com/thoughtowrks/feature_toggle_demo/config/NewFunction.java
@@ -0,0 +1,16 @@
+package com.thoughtowrks.feature_toggle_demo.config;
+
+import org.togglz.core.annotation.FeatureGroup;
+import org.togglz.core.annotation.Label;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@FeatureGroup
+@Label("Function Improvements")
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NewFunction {
+}
